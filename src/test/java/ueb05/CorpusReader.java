e13610a0ee2b674ab53c5c64733215e203ba1a55
Korbinian Riedhammer <korbinianr@gmail.com>
2018-04-25 10:00:59 +0200

    Fixed whitespace issue:

--- src/test/java/ueb05/CorpusReader.java
+++ src/test/java/ueb05/CorpusReader.java
@@ -11,8 +11,8 @@ class CorpusReader {
 
 	static Iterator<String> thesisTitleIterator() throws IOException {
 		ClassLoader classLoader = CorpusReader.class.getClassLoader();
-		File file = new File(classLoader.getResource("theses.txt").getFile());
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		InputStream is = classLoader.getResourceAsStream("theses.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
 		return new Iterator<String>() {
 			// keep first line ready
